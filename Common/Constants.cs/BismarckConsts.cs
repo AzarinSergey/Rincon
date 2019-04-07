346e164d0fc905d1e5eb6ca3772e03dff68945b1
Azarin Sergey <azarin.sergey@gmail.com>
2019-04-07 14:24:58 +0700

    cmn prj files

---
+++ Common/Constants.cs/BismarckConsts.cs
@@ -0,0 +1,12 @@
+﻿namespace Cmn.Constants
+{
+    /// <summary>
+    /// Общие костанты. Расшарил так для простоты решения.
+    /// </summary>
+    public class BismarckConsts
+    {
+        public const string MinioServerHost = "localhost:24001";
+        public const string MinioServerAccessKey = "AKIAIOSFODNN7EXAMPLE";
+        public const string MinioServerSecretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+    }
+}
---
+++ Common/Constants.cs/PumpConsts.cs
@@ -0,0 +1,12 @@
+﻿namespace Cmn.Constants
+{
+    public class PumpConsts
+    {
+        public const string MinioBucketName = "pump";
+        public const string EcnFilePrefix = "ecn_vr";
+        public const string VxFilePrefix = "vx";
+        public const string DataFileExtension = ".csv";
+        public const string OutputFileExtension = ".tsv";
+        public const string OutputFilePrefix = "output";
+    }
+}
